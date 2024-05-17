13f94c13b7465bf59923a20efdeeb2d74d08e02d
NaelNunes <107895361+NaelNunes@users.noreply.github.com>
2024-05-17 13:56:26 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#define TF 10
+int main()
+{
+    int vet[TF], i, num, pos, TL = 0;
+    
+    printf("Write the number to insert in vector: \n");
+    scanf("%d", &num);
+    
+    while(TL < TF && num > 0){
+        pos = 0;
+        
+        while(pos < TL && num > vet[pos])
+            pos++;
+        
+        for(i = TL; i > pos; i--)
+            vet[i] = vet[i - 1];
+        
+        vet[pos] = num;
+        TL++;
+        
+        if(TL < TF){
+            printf("Write the number to insert in vector: \n");
+            scanf("%d", &num);
+        }
+    }
+    
+    for(i = 0; i < TL; i++)
+        printf("%d", vet[i]);
+
+    return 0;
+}
 
